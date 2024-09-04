b4ad869b4a6b52b1fbcc6a88641c35a3704f4f19
Florencia Giménez <b3rserker1999@gmail.com>
2024-09-03 21:45:19 -0300

    Script

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-// En este archivo no utilizamos el evento "DOMContentLoaded", ya que se colocó el atributo "defer" en la importación del script,
-// que nos soluciona el problema de los elementos no cargados del DOM. Más info => https://www.w3schools.com/tags/att_script_defer.asp
+// URL que contiene los datos que queremos mostrar
+const DATA_URL = "json/data.json";
 
-const DATA_URL = "json/data.json"; // URL que contiene los datos que queremos mostrar
-
-const container = document.getElementById("container"); // "Traemos" utilizando el DOM el div de id "container" para colocar la información en él
+// Traemos utilizando el DOM el div de id "container" para colocar la información en él
+const container = document.getElementById("container");
 
 /**
  * Función que recibe por parámetro un array con los datos que se mostrarán en el DOM
@@ -11,11 +10,29 @@ const container = document.getElementById("container"); // "Traemos" utilizando 
  * imprime el campo "name" y el campo "lastname" separados por un espacio
  */
 function showData(dataArray) {
+  // Limpiar el contenido del contenedor antes de añadir los nuevos datos
+  container.innerHTML = '';
+
   // El for itera sobre los elementos del array
   for (const item of dataArray) {
-    // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
-    container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
+    // En la siguiente línea se utilizan "backticks" para armar el String.
+    container.innerHTML += `<p>${item.name} ${item.lastname}</p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
   }
 }
 
-// Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+// Realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+
+fetch(DATA_URL)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error' + response.statusText);
+    }
+    return response.json(); 
+  })
+  .then(data => {
+
+    showData(data.students); 
+  })
+  .catch(error => {
+    console.error('Hubo un error en el fetch', error);
+  });
